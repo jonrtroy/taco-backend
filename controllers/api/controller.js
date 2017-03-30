2077ed00134a11d9ceef42da27045ecc60d478e4
Jonathan Troy <jonrtroy@gmail.com>
2017-03-30 17:26:30 -0400

    getting info from yelp api

--- controllers/api/controller.js
+++ controllers/api/controller.js
@@ -1,3 +1,26 @@
-const controller = {};
+const yelp = require('yelp-fusion');
+const clientId = process.env.CLIENT_ID;
+const secretKey = process.env.SECRET;
+
+let controller = {};
+
+controller.taco = (req, res) => {
+  yelp.accessToken(clientId, secretKey).then(response => {
+    const client = yelp.client(response.jsonBody.access_token);
+
+    client.search({
+      categories: 'mexican,tacos',
+      location: `${req.params.lat}, ${req.params.lng}`,
+      radius: 750,
+      open_now: true
+    })
+    .then((results) => {
+      res.json(results.jsonBody.businesses);
+    })
+    .catch((err) => {
+      console.log('ERROR: ', err);
+    });
+  });
+}
 
 module.exports = controller;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+router.get ('/yelp/:lat/:lng', controller.taco);
 
 module.exports = router;
